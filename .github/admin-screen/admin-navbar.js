c1b1427dbd330a248ca244032013337a4e18bd62
Ashley <ashleylagman33@gmail.com>
2026-02-06 02:04:40 +0800

    added admin-screen and style css

---
+++ .github/admin-screen/admin-navbar.js
@@ -0,0 +1,22 @@
+document.getElementById("sidebar").innerHTML = ` 
+  <nav class="sidebar">     
+    <div>
+        <h2>UDICON <span style="color:#8fc96a;">Trading</span></h2>
+        <div class="menu">
+            <a href="admin-dashboard.html">Dashboard</a>
+            <a href="admin&inventory-search.html">Search</a>
+            <a href="admin&inventory-product.html">Product</a>
+            <a href="admin&inventory-supplier.html">Supplier</a>
+            <a href="admin-report.html">Reports</a>
+        </div>
+    </div>
+
+    <div class="bottom-menu">
+        <div class="menu">
+             <a href="admin-register.html">Register User</a>
+             <a href="admin-backup.html">Back-up</a>
+             <a href="admin-help.html">Help</a>
+        </div>
+        <button class="logout">Logout</button>
+    </div>
+</nav>   `;
