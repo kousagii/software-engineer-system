6574dc9dd053c32a125e82c51c9ee15857a7b1e8
Ashley <ashleylagman33@gmail.com>
2026-02-15 16:48:55 +0800

    shared admin-inventory folder

--- admin-screen/admin-navbar.js
+++ admin-screen/admin-navbar.js
@@ -3,19 +3,19 @@ document.getElementById("sidebar").innerHTML = `
     <div>
         <img id="logo" src="../assets/uc_logo.png" />
         <div class="menu">
-            <a href="admin-dashboard.html">Dashboard</a>
-            <a href="admin&inventory-search.html">Search</a>
-            <a href="admin&inventory-product.html">Product</a>
-            <a href="admin&inventory-supplier.html">Supplier</a>
-            <a href="admin-report.html">Reports</a>
+            <a href="../admin-screen/admin-dashboard.html">Dashboard</a>
+            <a href="../admin&inventory-screen/admin&inventory-search.html">Search</a>
+            <a href="../admin&inventory-screen/admin&inventory-product.html">Product</a>
+            <a href="../admin&inventory-screen/admin&inventory-supplier.html">Supplier</a>
+            <a href="../admin-screen/admin-report.html">Reports</a>
         </div>
     </div>
 
     <div class="bottom-menu">
         <div class="menu">
-             <a href="admin-register.html">Register User</a>
-             <a href="admin-backup.html">Back-up</a>
-             <a href="admin-help.html">Help</a>
+             <a href="../admin-screen/admin-register.html">Register User</a>
+             <a href="../admin-screen/admin-backup.html">Back-up</a>
+             <a href="../admin-screen/admin-help.html">Help</a>
         </div>
         <button class="logout">Logout</button>
     </div>
